e5df4c149a8c158dcdba45d2fe21f3274f25d879
sanjeev bhandari <sanjeevbhandari@sanjeevs-MacBook-Pro.local>
2024-04-10 06:47:02 +0545

    Sotry Section dynamic

--- resources/views/front/pages/story/_process.blade.php
+++ resources/views/front/pages/story/_process.blade.php
@@ -1,10 +1,9 @@
 @push('styles')
     <style>
         .how-we-process-container:not(:last-child):after{
-            content: "";
             position: absolute;
             height: var(--after-height);
-            width: calc(100% - 180px);
+            width: calc(100% - 180px - 360px);
             border: 1px dashed rgba(255, 255, 2550, 0.3);
             top: 100%;
         }
@@ -18,11 +17,16 @@
             border-left: 0;
             right: 180px;
         }
+        @media (min-width: 768px) {
+            .how-we-process-container:not(:last-child):after {
+                content: "";
+            }
+        }
     </style>
 @endpush
-<div class="how-we-process-section text-white">
+<div class="how-we-process-section text-white overflow-x-hidden">
     <h2 class="section-title text-center" data-aos="fade-up" data-aos-duration="1800">Our Oysters Journey</h2>
-    <div class="flex how-we-process-container relative right">
+    <div class="flex how-we-process-container py-4 lg:py-0 relative right" data-aos="fade-right" data-aos-duration="2000">
         <div class="how-we-process relative inline-block p-4 rounded-lg bg-gray-800 w-[360px]">
             <span class="font-bold text-lg">01.</span>
             <img src="{{Vite::asset('resources/images/front/process-1.png')}}" alt="process one" class="h-[140px] w-[240px] object-contain object-center mx-auto">
@@ -31,7 +35,7 @@
         </div>
         <div class="flex-1"></div>
     </div>
-    <div class="flex how-we-process-container relative left">
+    <div class="flex how-we-process-container py-4 lg:py-0 relative left" data-aos="fade-left" data-aos-duration="2000">
         <div class="flex-1"></div>
         <div class="how-we-process relative inline-block p-4 rounded-lg bg-gray-800 w-[360px]">
             <span class="font-bold text-lg">02.</span>
@@ -40,7 +44,7 @@
             <p class="text-sm mt-2 font-light">Our skilled technicians produce baby oysters (spat)</p>
         </div>
     </div>
-    <div class="flex how-we-process-container relative right">
+    <div class="flex how-we-process-container py-4 lg:py-0 relative right" data-aos="fade-right" data-aos-duration="2000">
         <div class="how-we-process relative inline-block p-4 rounded-lg bg-gray-800 w-[360px]">
             <span class="font-bold text-lg">03.</span>
             <img src="{{Vite::asset('resources/images/front/process-3.png')}}" alt="process one" class="h-[140px] w-[240px] object-contain object-center mx-auto">
@@ -49,7 +53,7 @@
         </div>
         <div class="flex-1"></div>
     </div>
-    <div class="flex how-we-process-container relative left">
+    <div class="flex how-we-process-container py-4 lg:py-0 relative left" data-aos="fade-left" data-aos-duration="2000">
         <div class="flex-1"></div>
         <div class="how-we-process relative inline-block p-4 rounded-lg bg-gray-800 w-[360px]">
             <span class="font-bold text-lg">04.</span>
@@ -58,7 +62,7 @@
             <p class="text-sm mt-2 font-light">We are the experts in small oyster spat husbandry, via 40+ years of development and learnings.</p>
         </div>
     </div>
-    <div class="flex how-we-process-container relative right">
+    <div class="flex how-we-process-container py-4 lg:py-0 relative right" data-aos="fade-right" data-aos-duration="2000">
         <div class="how-we-process relative inline-block p-4 rounded-lg bg-gray-800 w-[360px]">
             <span class="font-bold text-lg">05.</span>
             <img src="{{Vite::asset('resources/images/front/process-5.png')}}" alt="process one" class="h-[140px] w-[240px] object-contain object-center mx-auto">
@@ -67,7 +71,7 @@
         </div>
         <div class="flex-1"></div>
     </div>
-    <div class="flex how-we-process-container relative">
+    <div class="flex how-we-process-container py-4 lg:py-0 relative" data-aos="fade-left" data-aos-duration="2000">
         <div class="flex-1"></div>
         <div class="how-we-process relative inline-block p-4 rounded-lg bg-gray-800 w-[360px]">
             <span class="font-bold">06.</span>
