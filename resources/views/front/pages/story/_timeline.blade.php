6cc2c2f83a61b9cd0de97df140d2c5e810b0453a
sanjeev bhandari <sanjeevbhandari@sanjeevs-MacBook-Pro.local>
2024-04-10 06:32:32 +0545

    timeline section dynamic

--- resources/views/front/pages/story/_timeline.blade.php
+++ resources/views/front/pages/story/_timeline.blade.php
@@ -10,35 +10,21 @@
 @endpush
 <div class="timeline">
     <div class="mx-auto w-full h-full">
-        <div class="relative wrap overflow-hidden p-10 h-full">
+        <div class="relative wrap overflow-hidden pt-10 md:p-10 h-full">
             <div class="border-2-2 absolute border-opacity-20 border-white h-full border border-dashed" style="left: 50%"></div>
             @foreach($stories as $story)
-                <div class="mb-8 flex justify-between items-center w-full {{$loop->even ? 'right' : 'flex-row-reverse left'}}-timeline">
+                <div class="mb-8 flex justify-between items-center w-full {{$loop->even ? 'flex-col md:flex-row right' : 'flex-col md:flex-row-reverse left'}}-timeline">
                     <div class="order-1 w-5/12"></div>
                     <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-16 h-16 rounded-full cursor-pointer timeline-date">
                         <h1 class="mx-auto font-semibold text-lg text-white">{{$story->year}}</h1>
                     </div>
-                    <div class="order-1 bg-gray-800 rounded-lg shadow-xl w-5/12 px-6 py-4 opacity-0 timeline-content">
-                        <h3 class="mb-3 font-normal text-white text-xl text-capitalize">{{$story->title}}</h3>
-                        <p class="text-sm font-light leading-snug tracking-wide text-white text-opacity-50">{{$story->description}}</p>
+                    <div class="order-1 w-full md:w-5/12 min-h-[175px] md:min-h-[275px] lg:min-h-[230px] xl:min-h-[150px] mt-4 md:mt-0 opacity-0 timeline-content bg-gray-800 rounded-lg relative z-10 shadow-xl px-6 py-4">
+                            <h3 class="mb-3 font-normal text-white text-xl text-capitalize">{{$story->title}}</h3>
+                            <p class="text-sm font-light leading-snug tracking-wide text-white text-opacity-50">{{$story->description}}</p>
                     </div>
                 </div>
             @endforeach
             <!-- right timeline -->
-
-
-            <!-- left timeline -->
-            <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                <div class="order-1 w-5/12"></div>
-                <div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-16 h-16 rounded-full cursor-pointer timeline-date">
-                    <h1 class="mx-auto font-semibold text-lg text-white">2000</h1>
-                </div>
-                <div class="order-1 bg-gray-800 rounded-lg shadow-xl w-5/12 px-6 py-4 opacity-0 timeline-content">
-                    <h3 class="mb-3 font-normal text-white text-xl">Lorem Ipsum</h3>
-                    <p class="text-sm font-light leading-snug tracking-wide text-white text-opacity-50">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                </div>
-            </div>
-
         </div>
     </div>
 </div>
