63a935e682f516f9d2c49ef2a888abb48355e49d
sanjeev bhandari <sanjeevbhandari@sanjeevs-MacBook-Pro.local>
2024-04-11 08:27:23 +0545

    update

--- resources/views/front/pages/john-reserve/detail.blade.php
+++ resources/views/front/pages/john-reserve/detail.blade.php
@@ -28,14 +28,14 @@
 @section('content')
     <div class="bg-black text-white">
         <div class="container john-reserve-detail">
-            <div class="flex gap-24">
-                <div class="min-h-[500px] max-h-[700px] flex-1">
+            <div class="flex flex-col lg:flex-row lg:gap-24 pt-3">
+                <div class="lg:min-h-[500px] lg:max-h-[700px] flex-1">
                     <img src="{{asset("storage/uploads/$johnReserve->hero_image")}}" alt="John Reserve Details"
-                         class="cover-image h-full w-full">
+                         class="cover-image h-full h-[400px] w-[450px] lg:h-full lg:w-full mx-auto">
                 </div>
                 <div class="flex-1 pt-10">
-                    <div class="relative py-8 bg-black -left-3/4 pl-20 mb-8">
-                        <h1 class="hero-title whitespace-nowrap">{{$johnReserve->title}}</h1>
+                    <div class="relative py-6 lg:py-8 bg-black lg:-left-3/4 lg:pl-20 lg:mb-8">
+                        <h1 class="hero-title whitespace-nowrap text-center">{{$johnReserve->title}}</h1>
                     </div>
                     <p class="tracking-wide font-extralight leading-8 text-justify">
                         {{$johnReserve->description}}
@@ -43,36 +43,36 @@
                 </div>
             </div>
         </div>
-        <div class="container relative py-12">
-           @include('front.pages.john-reserve._range_slider', [
-                    'slider_title' => 'Umani',
+         <div class="container relative py-12">
+            @include('front.pages.john-reserve._range_slider', [
+                     'slider_title' => 'Umani',
+                     'label_1' => 'Low',
+                     'label_2' => 'Medium',
+                     'label_3' => 'High',
+                     'value' => $johnReserve->umami,
+            ])
+             @include('front.pages.john-reserve._range_slider', [
+                    'slider_title' => 'saltiness',
                     'label_1' => 'Low',
                     'label_2' => 'Medium',
                     'label_3' => 'High',
-                    'value' => $johnReserve->umami,
+                    'value' => $johnReserve->saltiness,
            ])
-            @include('front.pages.john-reserve._range_slider', [
-                   'slider_title' => 'saltiness',
-                   'label_1' => 'Low',
-                   'label_2' => 'Medium',
-                   'label_3' => 'High',
-                   'value' => $johnReserve->saltiness,
-          ])
-            @include('front.pages.john-reserve._range_slider', [
-                   'slider_title' => 'texture',
-                   'label_1' => 'Low',
-                   'label_2' => 'Medium',
-                   'label_3' => 'High',
-                   'value' => $johnReserve->texture,
-          ])
-            @include('front.pages.john-reserve._range_slider', [
-                   'slider_title' => 'finish',
-                   'label_1' => 'Low',
-                   'label_2' => 'Medium',
-                   'label_3' => 'High',
-                   'value' => $johnReserve->finish,
-          ])
-        </div>
+             @include('front.pages.john-reserve._range_slider', [
+                    'slider_title' => 'texture',
+                    'label_1' => 'Low',
+                    'label_2' => 'Medium',
+                    'label_3' => 'High',
+                    'value' => $johnReserve->texture,
+           ])
+             @include('front.pages.john-reserve._range_slider', [
+                    'slider_title' => 'finish',
+                    'label_1' => 'Low',
+                    'label_2' => 'Medium',
+                    'label_3' => 'High',
+                    'value' => $johnReserve->finish,
+           ])
+         </div>
     </div>
 @endsection
 @push('scripts')
